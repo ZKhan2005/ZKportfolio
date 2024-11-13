153937b694f1de788aa45846ec06f568fb72eeb3
WajahatAli3218664 <assim345678@gmail.com>
2024-11-13 20:15:06 +0500

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,127 @@
+
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Linkedin",
+    src: "/icons8-linkedin.svg",
+    href: "https://www.linkedin.com/in/z-khan-2b309b300/",
+  },
+  {
+    name: "GitHub",
+    src: "/icons8-github.svg",
+    href: "https://github.com/ZKhan2005",
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    href: "https://discord.com/channels/@me",
+  },
+];
+export const Projects = [
+  {
+    title: "Dark Mode Calculator ",
+    text: "A simple, responsive calculator built with HTML, CSS, and JavaScript, featuring a toggle between dark and light modes for user-friendly operation",
+    src: "/dark.png",
+    Link:"https://codsoft-cal-task-3.vercel.app/?vercelToolbarCode=4p4_kd2_lPqGwhe"
+  },
+  {
+    title: "Modern Portfolio Website",
+    text: "This tool allows users to create, customize, and edit their resumes easily. With a user-friendly interface, it enables real-time updates and formatting options, making resume-building efficient and personalized",
+    src: "/edit.png",
+    Link:"https://hackathon-milestone--black.vercel.app/"
+  },
+  {
+    title: "Static Interactive Resume",
+    text: "Built a responsive To-Do List app with React, Context API, and Tailwind CSS, featuring task management with add, edit, and delete functions for a seamless user experience. Hosted on Netlify.",
+    Link:"https://milestones-1-nine.vercel.app/",
+    src: "cv.png"
+  },
+  {
+    title: "Next.js: The React Framework for Production",
+    text: "Next.js is a powerful React framework for building high-performance, scalable web applications with ease. It offers server-side rendering, static site generation, and optimized performance, making it ideal for projects of any size. It pulls recipe information like the title, and instructions using the Edamam API.",
+    src: "/next.jpg",
+    Link:"https://nextjs.org/"
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/skills",
+    icon: RxPerson,
+    link: "/skills",
+  },
+  {
+    name: "/projects",
+    icon: RxDashboard,
+    link: "/projects",
+  },
+  {
+    name: "/contact",
+    icon: RxClipboard,
+    link: "/contact",
+  },
+];
